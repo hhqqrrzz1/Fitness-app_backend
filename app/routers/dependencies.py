c718419f4336904f2bc3518c85a13cd85cfc472d
Nikita <nikita@debian.nikita>
2025-03-25 21:40:48 +0300

    fix

--- app/routers/dependencies.py
+++ app/routers/dependencies.py
@@ -1,7 +1,6 @@
 from typing import Annotated
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends
-from fastapi import HTTPException, status
 
 from app.routers.auth import get_current_user
 from app.backend.db_depends import get_db
